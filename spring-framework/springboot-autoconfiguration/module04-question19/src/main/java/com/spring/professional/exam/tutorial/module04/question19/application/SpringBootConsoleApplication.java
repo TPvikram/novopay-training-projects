48d52d65e69c748df238b99496b23afe03a6afaa
Vikram Parthiban <thiruvikraman@novopay.in>
2021-03-01 15:11:24 +0530

    added code for spring-test

--- spring-framework/springboot-autoconfiguration/module04-question19/src/main/java/com/spring/professional/exam/tutorial/module04/question19/application/SpringBootConsoleApplication.java
+++ spring-framework/springboot-autoconfiguration/module04-question19/src/main/java/com/spring/professional/exam/tutorial/module04/question19/application/SpringBootConsoleApplication.java
@@ -7,14 +7,15 @@ import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
-//@SpringBootApplication(scanBasePackageClasses = {SpringBootConsoleApplication.class, ServiceB.class})
+//@SpringBootApplication
+@SpringBootApplication(scanBasePackageClasses = {SpringBootConsoleApplication.class, ServiceB.class})
 public class SpringBootConsoleApplication implements CommandLineRunner {
 
     @Autowired
     private ServiceA serviceA;
-    //@Autowired
-   // private ServiceB serviceB;
+
+    @Autowired
+   private ServiceB serviceB;
 
     public static void main(String[] args) {
         SpringApplication.run(SpringBootConsoleApplication.class, args);
